4b5675d93c4bc8a2748b0a76a17ac5daa4d40ccf
clbokea <clbo@kea.dk>
2024-03-13 09:13:13 +0100

    script.js change 4

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $().ready(function() {
 	$("h1:first").hide()
 
-	$(".markdown-body").prepend("<div style='text-align:right; margin:40px 0 40px 0; color:#888888'><small>Software Arkitektur efterår 2024 - Kompendie - clbo@kea.dk</small></div>");
+	$(".markdown-body").prepend("<div style='text-align:center; margin:40px 0 40px 0; color:#888888'><small>Software Arkitektur efterår 2024 - Kompendie - clbo@kea.dk</small></div>");
 
-	$(".markdown-body").append("<div style='text-align:right; margin-top:60px; color:#888888'><small>Software Arkitektur efterår 2024 - Kompendie - clbo@kea.dk</small></div>");
+	$(".markdown-body").append("<div style='text-align:center; margin-top:60px; color:#888888'><small>Software Arkitektur efterår 2024 - Kompendie - clbo@kea.dk</small></div>");
 });
 
 
