f45775a51a94f1d4e78555fd932f2508fb46f794
clbokea <clbo@kea.dk>
2024-03-13 17:28:44 +0100

    scriptjs table add 10

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ $(document).ready(function() {
     $("h1:first").hide();
 
     // Tilføjer en div med tekst øverst i .markdown-body
-    $(".markdown-body").prepend("<div style='text-align:center; margin:40px 0 40px 0; color:#888888'><small>Kompendie - Software Arkitektur & Cloud efterår 2024 xx</small></div>");
+    $(".markdown-body").prepend("<div style='text-align:center; margin:40px 0 40px 0; color:#888888'><small>Kompendie - Software Arkitektur & Cloud efterår 2024 xxxxxx</small></div>");
 
     // Tilføjer en div med tekst nederst i .markdown-body
     $(".markdown-body").append("<div style='text-align:center; margin-top:60px; color:#888888'><small>Kompendie - Software Arkitektur & Cloud efterår 2024 - clbo@kea.dk - licence: MIT</small></div>");
