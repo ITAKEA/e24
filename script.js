f8e494ab8559c8b7b0c5d984d7ada18f5c32b985
clbokea <clbo@kea.dk>
2024-03-13 14:38:28 +0100

    scriptjs change

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $().ready(function() {
 	$("h1:first").hide()
 
-	$(".markdown-body").prepend("<div style='text-align:center; margin:40px 0 40px 0; color:#888888'><small>Software Arkitektur efterår 2024 - Kompendie - clbo@kea.dk</small></div>");
+	$(".markdown-body").prepend("<div style='text-align:center; margin:40px 0 40px 0; color:#888888'><small>Kompendie - Software Arkitektur & Cloud efterår 2024</small></div>");
 
-	$(".markdown-body").append("<div style='text-align:center; margin-top:60px; color:#888888'><small>Software Arkitektur efterår 2024 - Kompendie - clbo@kea.dk</small></div>");
+	$(".markdown-body").append("<div style='text-align:center; margin-top:60px; color:#888888'><small>Kompendie - Software Arkitektur & Cloud efterår 2024 - clbo@kea.dk - licence: MIT</small></div>");
 });
 
 
